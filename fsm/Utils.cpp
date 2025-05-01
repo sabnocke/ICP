e9e01a6e819e59802ec6a61b792560d633b3dc88
Michal Hloch <hloch.michal@gmail.com>
2025-05-01 17:36:58 +0200

    Added ToString

--- fsm/Utils.cpp
+++ fsm/Utils.cpp
@@ -3,9 +3,11 @@
 //
 
 #include "Utils.h"
+#include <algorithm>
 #include <cctype>
 #include <locale>
-#include <algorithm>
+#include <optional>
+#include <sstream>
 
 std::string Utils::TrimLeft(const std::string &str) {
   const std::string pattern = " \f\n\r\t\v";
@@ -17,15 +19,13 @@ std::string Utils::TrimRight(const std::string &str) {
   return str.substr(0, str.find_last_not_of(pattern) + 1);
 }
 
-std::string Utils::Trim(const std::string &str) {
-  return TrimLeft(TrimRight(str));
-}
+std::string Utils::Trim(const std::string &str) { return TrimLeft(TrimRight(str)); }
 
 std::string Utils::Purify(const std::string &str) {
-  std::string result = "";
-  for (const unsigned c : str) {
+  std::string result;
+  for (const unsigned char c: str) {
     if (!isspace(c)) {
-      result += c;
+      result += std::to_string(c);
     }
   }
   return result;
@@ -45,3 +45,26 @@ std::string Utils::Remove(const std::string &str, const std::string &substr) {
   }
   return result;
 }
+
+
+template<typename T>
+std::optional<std::string> Utils::ToStringOpt(const T value) {
+  if constexpr (IsStdString<T>) {
+    return value;
+  } else if constexpr (IsStringView<T>) {
+    return std::string(value);
+  } else if constexpr (IsCString<T>) {
+    return value == nullptr ? "" : std::string(value);
+  } else if constexpr (std::same_as<std::remove_cvref_t<T>, bool>) {
+    std::ostringstream oss;
+    oss << std::boolalpha << value;
+    return oss.str();
+  } else if constexpr (Streamable<T>) {
+    std::ostringstream oss;
+    oss << value;
+    return oss.str();
+  } else {
+    return std::nullopt;
+  }
+}
+
--- fsm/Utils.h
+++ fsm/Utils.h
@@ -1,10 +1,22 @@
-//
-// Created by ReWyn on 30.04.2025.
-//
-
 #pragma once
 #include <string>
+#include <optional>
+#include <concepts>
 
+template<typename T>
+concept Streamable = requires(std::ostream &os, const T &t)
+{
+  {os << t} -> std::convertible_to<std::ostream &>;
+};
+
+template<typename T>
+concept IsStdString = std::same_as<std::remove_cvref_t<T>, std::string>;
+
+template<typename T>
+concept IsCString = std::is_same_v<const char *, std::decay_t<T>> || std::is_same_v<char *, std::decay_t<T>>;
+
+template<typename T>
+concept IsStringView = std::same_as<std::remove_cvref_t<T>, std::string_view>;
 
 class Utils {
 public:
@@ -14,6 +26,8 @@ public:
   static std::string Purify(const std::string &str);
   static std::string Remove(const std::string &str, char c);
   static std::string Remove(const std::string &str, const std::string &substr);
+  template<typename T>
+  std::optional<std::string> Utils::ToStringOpt(const T value);
 private:
   const std::string whitespacePattern = " \f\n\r\t\v";
 };
