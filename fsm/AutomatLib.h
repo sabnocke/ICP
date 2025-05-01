12c76d13c7c83a341ea8634d59540d299cf27dcb
Michal Hloch <hloch.michal@gmail.com>
2025-05-01 17:40:34 +0200

    Changed AutomatLib to work with strings

--- fsm/AutomatLib.h
+++ fsm/AutomatLib.h
@@ -12,23 +12,24 @@
 struct Transition {
   std::string from;
   std::string to;
-  std::function<void()> condition;
-  int timeoutMs;
+  std::string input;
+  std::string cond;
+  std::string delay;
 };
 struct State {
   std::string name;
-  std::function<void()> action;
+  // std::function<void()> action;
+  std::string action;
 };
 
 class Automat {
 public:
-  void addState(const std::string &name, std::function<void()> action);
-  void addTransition(const std::string &from, const std::string &to, std::function<bool()> condition, int timeoutMs = 0);
-  void runOnce();
-  void setInput(const std::string &name, const std::string &value);
+  void addState(const std::string &name, const std::string &action);
+  void addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay);
   void addVariable(const std::string &type, const std::string &name, const std::string &value);
-  std::string getInput(const std::string &name);
   std::optional<std::string> valueof(const std::string &name);
+  template <typename T>
+  bool Automat::output(const std::string &outputName, const T &value);
   std::string Name;
   std::string Comment;
 private:
