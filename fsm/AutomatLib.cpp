86112a986ef8ec5843f444f0ea207e7d42f1e79d
Michal Hloch <hloch.michal@gmail.com>
2025-05-01 17:40:06 +0200

    Considering the change of how the execution is done, removed unused code

--- fsm/AutomatLib.cpp
+++ fsm/AutomatLib.cpp
@@ -10,57 +10,39 @@
 #include <optional>
 #include <ostream>
 #include <string>
+#include <utility>
 #include <vector>
+
+#include "Utils.h"
 
 
 // #include "Stopwatch.h" // will be useful later or elsewhere
 using namespace std::chrono;
 
-void Automat::addState(const std::string &name, std::function<void()> action) {
+void Automat::addState(const std::string &name, const std::string &action) {
   states[name] = State{name, action};
-  if (firstRun) {
-    firstRun = false;
-    currentState = name;
-  }
 }
 
-void Automat::addTransition(const std::string &from, const std::string &to, std::function<bool()> condition, const int timeoutMs) {
-  transitions.push_back({from, to, condition, timeoutMs});
+void Automat::addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay) {
+  transitions.push_back({from, to, input, cond, delay});
 }
 
 void Automat::addVariable(const std::string &type, const std::string &name, const std::string &value) {
   variables[name] = std::make_pair(type, value);
 }
 
-
-void Automat::runOnce() {
-  if (states.contains(currentState)) {
-    std::cerr << "AutomatLib::runOnce(): State not found" << std::endl; //TODO this should output to gui
-    return;
-  }
-
-  for (const auto &transition : transitions) {
-    //TODO transition.condition() is void and also no parsing will be done here
-    if (transition.from == currentState && transition.condition()) {
-      std::cout << "Transition from: " << transition.from << " to " << transition.to << std::endl;
-      currentState = transition.to;
-      states[currentState].action();
-      return;
-    }
-  }
-}
-
-void Automat::setInput(const std::string &name, const std::string &value) {
-  inputs[name] = value;
-}
-std::string Automat::getInput(const std::string &name) {
-  //TODO verify that this actually can return something (effectively what valueof does)
-  return inputs[name];
-}
-
 std::optional<std::string> Automat::valueof(const std::string &name) {
-  if (const auto input = inputs.find(name); input != inputs.end()) {
-    return input->second;
+  if (inputs.contains(name)) {
+    return inputs[name];
   }
   return std::nullopt;
+}
+
+template <typename T>
+bool Automat::output(const std::string &outputName, const T &value) {
+  if (outputs.contains(outputName)) {
+    outputs[outputName] = Utils::ToStringOpt(value);
+    return true;
+  }
+  return false;
 }
