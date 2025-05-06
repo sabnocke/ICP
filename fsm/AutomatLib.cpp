8a9a2a709411e5fde86de30d220a18929d98d1b3
Michal Hloch <hloch.michal@gmail.com>
2025-05-07 00:40:44 +0200

    Expanded support for interaction with automat definition

--- fsm/AutomatLib.cpp
+++ fsm/AutomatLib.cpp
@@ -1,7 +1,3 @@
-//
-// Created by ReWyn on 28.04.2025.
-//
-
 #include "AutomatLib.h"
 #include <bits/ios_base.h>
 #include <chrono>
@@ -14,35 +10,48 @@
 #include <vector>
 
 #include "Utils.h"
+#include "parser.h"
 
 
-// #include "Stopwatch.h" // will be useful later or elsewhere
-using namespace std::chrono;
-
-void Automat::addState(const std::string &name, const std::string &action) {
-  states[name] = State{name, action};
-}
-
-void Automat::addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay) {
-  transitions.push_back({from, to, input, cond, delay});
-}
-
-void Automat::addVariable(const std::string &type, const std::string &name, const std::string &value) {
-  variables[name] = std::make_pair(type, value);
-}
-
-std::optional<std::string> Automat::valueof(const std::string &name) {
-  if (inputs.contains(name)) {
-    return inputs[name];
+namespace AutomatLib {
+  void Automat::addState(const std::string &name, const std::string &action) {
+    states[name] = State{name, action};
   }
-  return std::nullopt;
-}
-
-template <typename T>
-bool Automat::output(const std::string &outputName, const T &value) {
-  if (outputs.contains(outputName)) {
-    outputs[outputName] = Utils::ToStringOpt(value);
-    return true;
+  void Automat::addState(const std::tuple<std::string, std::string> &result) {
+    addState(std::get<0>(result), std::get<1>(result));
   }
-  return false;
+
+  void Automat::addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay) {
+    transitions.push_back({from, to, input, cond, delay});
+  }
+
+  void Automat::addTransition(const Parser::ParseTransitionRecord &result) {
+    addTransition(result.from, result.to, result.input, result.cond, result.delay);
+  }
+
+  void Automat::addVariable(const std::string &type, const std::string &name, const std::string &value) {
+    variables[name] = std::make_pair(type, value);
+  }
+  void Automat::addVariable(const Parser::ParseVariableRecord &result) {
+    addVariable(result.type, result.name, result.value);
+  }
+
+  //TODO move this to helper function class for AutomatRuntime
+  std::optional<std::string> Automat::valueof(const std::string &name) {
+    if (inputs.contains(name)) {
+      return inputs[name];
+    }
+    return std::nullopt;
+  }
+
+
+  //TODO move this to helper function class for AutomatRuntime
+  template <typename T>
+  bool Automat::output(const std::string &outputName, const T &value) {
+    if (outputs.contains(outputName)) {
+      outputs[outputName] = Utils::ToStringOpt(value);
+      return true;
+    }
+    return false;
+  }
 }
--- fsm/AutomatLib.h
+++ fsm/AutomatLib.h
@@ -8,37 +8,45 @@
 #include <optional>
 #include <string>
 
+namespace Parser {
+  struct ParseTransitionRecord;
+  struct ParseVariableRecord;
+}
 
-struct Transition {
-  std::string from;
-  std::string to;
-  std::string input;
-  std::string cond;
-  std::string delay;
-};
-struct State {
-  std::string name;
-  // std::function<void()> action;
-  std::string action;
-};
+namespace AutomatLib {
+  struct Transition {
+    std::string from;
+    std::string to;
+    std::string input;
+    std::string cond;
+    std::string delay;
+  };
+  struct State {
+    std::string name;
+    std::string action;
+  };
 
-class Automat {
-public:
-  void addState(const std::string &name, const std::string &action);
-  void addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay);
-  void addVariable(const std::string &type, const std::string &name, const std::string &value);
-  std::optional<std::string> valueof(const std::string &name);
-  template <typename T>
-  bool Automat::output(const std::string &outputName, const T &value);
-  std::string Name;
-  std::string Comment;
-private:
-  std::unordered_map<std::string, State> states;
-  std::vector<Transition> transitions;
-  std::unordered_map<std::string, std::string> inputs;
-  std::unordered_map<std::string, std::string> outputs;
-  std::unordered_map<std::string, std::pair<std::string, std::string>> variables;
-  std::string currentState;
-  bool firstRun = true;
-};
+  class Automat {
+  public:
+    void addState(const std::string &name, const std::string &action);
+    void addState(const std::tuple<std::string, std::string> &result);
+    void addTransition(const std::string &from, const std::string &to, const std::string &input, const std::string &cond, const std::string &delay);
+    void addTransition(const Parser::ParseTransitionRecord &result);
+    void addVariable(const std::string &type, const std::string &name, const std::string &value);
+    void addVariable(const Parser::ParseVariableRecord &result);
+    std::optional<std::string> valueof(const std::string &name);
+    template <typename T>
+    bool output(const std::string &outputName, const T &value);
+    std::string Name;
+    std::string Comment;
+  private:
+    std::unordered_map<std::string, State> states;
+    std::vector<Transition> transitions;
+    std::unordered_map<std::string, std::string> inputs;
+    std::unordered_map<std::string, std::string> outputs;
+    std::unordered_map<std::string, std::pair<std::string, std::string>> variables;
+    std::string currentState;
+    bool firstRun = true;
+  };
 
+}
