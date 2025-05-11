0bc8e14c5775a95b43bed22393ca09b11a237a0e
Michal Hloch <hloch.michal@gmail.com>
2025-05-11 20:27:23 +0200

    This probably wont be used

--- fsm/Timing-linux.cpp
+++ fsm/Timing-linux.cpp
@@ -1,10 +1,37 @@
 #include "Timing.h"
+#include <sys/timerfd.h>
+#include <sys/time.h>
+#include <iostream>
+#include <unistd.h>
+#include <string>
 
-#include <absl/strings/str_format.h>
-#include <absl/synchronization/mutex.h>
-
-#include <optional>
 
 namespace Timing {
+void Timer::Wait(size_t msTimeout) {
+  int stdin = fileno(stdin);
 
+  fd_set read_fds;
+  FD_ZERO(&read_fds);
+  FD_SET(stdin, &read_fds);
+
+  msTimeout.tv_sec = msTimeout / 1000;
+  msTimeout.tv_usec = (msTimeout % 1000) * 1000;
+
+  int ready = select(stdin + 1, &read_fds, NULL, NULL, &msTimeout);
+
+  if (ready == -1) {
+    perror("select (Unix)");
+    return;
+  } else if (ready == 0) {
+    std::cerr << "Timeout (Unix)" << std::endl;
+    //TODO handle timeout
+  } else {
+    if (FD_ISSET(stdin, &read_fds)) {
+      std::cout << "Data available on stdin (Unix):\n";
+      std::string input;
+      std::getline(std::cin, input);
+      std::cout << "Received: " << input << "\n";
+    }
+  }
+}
 }  // namespace Timing
