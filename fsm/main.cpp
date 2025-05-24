d3e8dcfd85feed2a226ec8038eae142e3784253b
Michal Hloch <hloch.michal@gmail.com>
2025-05-24 14:39:36 +0200

    Update

--- fsm/main.cpp
+++ fsm/main.cpp
@@ -1,10 +1,8 @@
-
 #include <absl/flags/flag.h>
 #include <absl/log/absl_log.h>
 #include <absl/log/globals.h>
 #include <absl/log/initialize.h>
 #include <absl/strings/str_format.h>
-#include <spdlog/spdlog.h>
 
 #include <cassert>
 #include <fstream>
@@ -14,12 +12,11 @@
 #include "ParserLib.h"
 #include "external/sol.hpp"
 
-//TODO add log sink to a file
 
 void parserTest() {
   ParserLib::Parser parser;
 
-  auto [name, cond] = parser.parseState("IDLE : [ output(\"out\", 0) ]");
+  auto [name, cond] = parser.parseState("state IDLE [ output(\"out\", 0) ]");
   std::cout << absl::StrFormat("state %s has action %s\n", name, cond);
 
   std::cout << parser.parseVariable("    int timeout = 5000") << std::endl;
@@ -30,22 +27,12 @@ void parserTest() {
 
   std::cout << Utils::Quote("IDLE : { output(\"out\", 0) }") << std::endl;
 
-  //? Get is necessary for getting the value if conv was directly printed it would output correctly 10
   auto conv = Utils::StringToNumeric<int>("10");
   if (auto c = conv; c.has_value()) {
     std::cout << c.value() << std::endl;
   }
 }
-//
-//   auto r = parser.parseAutomat("fsmDefinition.txt");
-//   std::cout << r.states.Size() << std::endl;
-//   std::cout << r.states << std::endl;
-//   std::cout << "IN: " << absl::StrJoin(r.inputs, ", ") << std::endl;
-//   std::cout << "OUT: " << absl::StrJoin(r.outputs, ", ") << std::endl;
-//   std::cout << r.variables << std::endl;
-//   std::cout << r.transitions << std::endl;
-// }
-//
+
 void luaTest() {
   sol::state lua{};
   int x = 0;
@@ -57,12 +44,17 @@ void luaTest() {
 
 int main(const int argc, char** argv) {
   if (argc < 2) {
-    spdlog::critical("Requires path to valid fsm definition");
+    ABSL_LOG(ERROR) << "Requires path to valid fsm definition";
     return 1;
   }
+
+  absl::InitializeLog();
+  // absl::SetMinLogLevel(absl::LogSeverityAtLeast::kInfo); //<-- this doesn't do anything of use
+
   try {
-    spdlog::info("Hello World");
-  } catch (const Utils::ProgramTermination) {
+    ABSL_LOG(INFO) << "Hello, World!";
+    parserTest();
+  } catch (const Utils::ProgramTermination&) {
     return 1;
   }
   // sol::state lua{};
